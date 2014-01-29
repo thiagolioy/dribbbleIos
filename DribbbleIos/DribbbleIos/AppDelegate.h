3202d99d542e1cea6e264e15d450f7b04a4cd951
Thiago Lioy <lioyufrj@gmail.com>
2014-01-29 16:37:47 -0200

    initial commit

---
+++ DribbbleIos/DribbbleIos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DribbbleIos
+//
+//  Created by Thiago Lioy on 1/29/14.
+//  Copyright (c) 2014 Thiago Lioy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DribbbleIos/DribbbleIos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DribbbleIos
+//
+//  Created by Thiago Lioy on 1/29/14.
+//  Copyright (c) 2014 Thiago Lioy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
