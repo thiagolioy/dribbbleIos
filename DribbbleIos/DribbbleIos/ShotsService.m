162341fac343f2fe5aad20dc365276046b46ee44
Thiago Lioy <lioyufrj@gmail.com>
2014-02-02 11:39:30 -0200

    add concrete implementation , going to dribbble api

--- DribbbleIos/DribbbleIos/ShotsService.m
+++ DribbbleIos/DribbbleIos/ShotsService.m
@@ -7,13 +7,26 @@
 //
 
 #import "ShotsService.h"
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworking/AFNetworkActivityIndicatorManager.h>
+#import "ShotsListResponse.h"
+
+#define LIST_URL @"shots/%@"
 
 @implementation ShotsService
 
 -(void)fetchShotsList:(NSString *)listType
            completion:(FetchShotsListCompletionBlock)block{
-
-
+    
+    AFNetworkActivityIndicatorManager.sharedManager.enabled = YES;
+    AFHTTPRequestOperationManager *manager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://api.dribbble.com/"]];
+    NSString *url = [NSString stringWithFormat:LIST_URL,listType];
+    [manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        ShotsListResponse *response = [ShotsListResponse shotsListResponseWithDict:responseObject];
+        block(response.shots);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+         block([NSArray array]);
+    }];
 }
 
 @end
