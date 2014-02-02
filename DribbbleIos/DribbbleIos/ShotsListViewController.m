5d1926ac12876da3e8b703c0af3889b772c3cd1b
Thiago Lioy <lioyufrj@gmail.com>
2014-02-02 16:51:32 -0200

    removing non using methods

--- DribbbleIos/DribbbleIos/ShotsListViewController.m
+++ DribbbleIos/DribbbleIos/ShotsListViewController.m
@@ -44,13 +44,6 @@ static NSString *ShotCellID = @"ShotCellID";
     }];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return _shotsArray.count;
 }
