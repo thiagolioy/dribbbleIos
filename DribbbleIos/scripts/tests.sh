5be0f2acf9409b633d6fe40cb15d04f2f88b4297
Thiago Lioy <lioyufrj@gmail.com>
2014-02-10 14:31:11 -0200

    ajustando scripts

--- DribbbleIos/scripts/tests.sh
+++ DribbbleIos/scripts/tests.sh
@@ -1,2 +1,2 @@
-xcodebuild -workspace ../DribbbleIos.xcworkspace -scheme DribbbleIosTests-jenkins -sdk iphonesimulator TEST_AFTER_BUILD=YES clean build test
+xcodebuild -workspace DribbbleIos.xcworkspace -scheme DribbbleIosTests-jenkins -sdk iphonesimulator TEST_AFTER_BUILD=YES clean build test
 
